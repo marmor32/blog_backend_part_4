fdffe25d58076877fbe63e31711b2d2f0e59e52a
Dvir marmor <marmordvir@gmail.com>
2021-08-08 08:25:22 +0300

    before yeshiva

--- controllers/login.js
+++ controllers/login.js
@@ -23,10 +23,9 @@ loginRouter.post('/', async (request, response) => {
     }
 
     const token = jwt.sign(userForToken, process.env.SECRET) // add token expire {expiresIn: 60*60 }
-
     response
         .status(200)
-        .send({token, username: user.username, name: user.name})
+        .send({token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
